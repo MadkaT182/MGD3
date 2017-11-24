406093eae92dffd586d1e8ffa62a11b2b42deb7d
unknown <madcat182@gmail.com>
2017-11-24 11:56:41 -0500

    Metrics update

--- Graphics/ScreenTitleMenu scroll.lua
+++ Graphics/ScreenTitleMenu scroll.lua
@@ -1,8 +1,7 @@
 local children = {
 	LoadFont("PaneDisplay text") ..{
 		Text=THEME:GetString( 'ScreenTitleMenu', Var("GameCommand"):GetText() );
-		InitCommand=cmd();
-		GainFocusCommand=cmd(diffuse,color("#929395");zoom,.33);
+		GainFocusCommand=cmd(diffuse,color("#929395");zoom,.34);
 		LoseFocusCommand=cmd(diffuse,color("#FFFFFF");zoom,.29);
 		DisabledCommand=cmd(diffuse,0.5,0.5,0.5,1);
 	};
